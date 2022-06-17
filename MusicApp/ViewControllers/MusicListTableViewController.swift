6cf922ea390681667f097869354c9b9c51373918
Artur <alferovnet@mail.ru>
2022-06-17 15:44:19 +0300

    add func in main VC

--- MusicApp/ViewControllers/MusicListTableViewController.swift
+++ MusicApp/ViewControllers/MusicListTableViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class MusicListTableViewController: UITableViewController {
+    
+    var tracks: [MusicModel] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchTracksFromNM()
 
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-       
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 0
+        tracks.count
     }
 
     
@@ -79,4 +76,20 @@ class MusicListTableViewController: UITableViewController {
     }
     */
 
+}
+
+extension MusicListTableViewController {
+    func fetchTracksFromNM(){
+        NetworkManager.shared.fetchTracks { result in
+            switch result {
+            case .success(let tracks):
+                self.tracks = tracks
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
