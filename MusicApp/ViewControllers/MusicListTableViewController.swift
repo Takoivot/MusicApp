c628175c952a4b9bd6b7d4b03114e1514dda23b4
MakGris <maxgrishenko@gmail.com>
2022-06-20 21:03:35 +0700

    Add tabBar, tune tableview rows.

--- MusicApp/ViewControllers/MusicListTableViewController.swift
+++ MusicApp/ViewControllers/MusicListTableViewController.swift
@@ -12,29 +12,32 @@ class MusicListTableViewController: UITableViewController {
     var tracks: MusicModel? = nil
     
     let searchController = UISearchController(searchResultsController: nil)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 100
+        tableView.separatorStyle = .none
         fetchTracksFromNM()
         setupSearchBar()
-
+        configureNavBar()
+        
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         tracks?.results.count ?? 0
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "music", for: indexPath) as? MusicListViewCell else {return UITableViewCell()}
         guard let track = tracks?.results[indexPath.row] else {return UITableViewCell()}
         cell.settingsCell(with: track)
+        cell.backgroundColor = .black
         return cell
     }
-
+    
 }
 
 extension MusicListTableViewController {
@@ -59,6 +62,16 @@ extension MusicListTableViewController : UISearchBarDelegate{
         navigationController?.navigationBar.prefersLargeTitles = true
         searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchBar.delegate = self
+    }
+    private func configureNavBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .black
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
