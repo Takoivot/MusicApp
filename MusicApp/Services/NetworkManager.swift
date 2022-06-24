8eae9d367d34b549c2d7f619f78eb07a6c2628bd
Artur <alferovnet@mail.ru>
2022-06-24 14:12:26 +0300

    Update NetworkManager.swift
    add fetchImage func

--- MusicApp/Services/NetworkManager.swift
+++ MusicApp/Services/NetworkManager.swift
@@ -38,6 +38,22 @@ class NetworkManager {
         }.resume()
     }
     
+    func fetchImage(with track: Tracks, completion: @escaping (Result<Data,NetworkError>) -> Void){
+        DispatchQueue.global().async {
+            guard let url = URL(string: track.artworkUrl60 ?? "" ) else {
+                completion(.failure(.invalidURL))
+                return
+            }
+            if let imageData = try? Data(contentsOf: url){
+                completion(.success(imageData))
+                return
+            } else {
+                completion(.failure(.invalidData))
+                return
+            }
+        }
+    }
+    
     private init() {}
     
 }
