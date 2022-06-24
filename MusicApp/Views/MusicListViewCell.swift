890f4c6076b9c55d124bfd015cda56309ce799b2
Artur <alferovnet@mail.ru>
2022-06-24 14:20:44 +0300

    Update MusicListViewCell.swift
    add  configure func in Custom Cell

--- MusicApp/Views/MusicListViewCell.swift
+++ MusicApp/Views/MusicListViewCell.swift
@@ -11,10 +11,22 @@ class MusicListViewCell: UITableViewCell {
     
     @IBOutlet var artistName: UILabel!
     @IBOutlet var songName: UILabel!
+    @IBOutlet var imageSong: UIImageView!
     
     func settingsCell(with track: Tracks ){
         artistName.text = track.artistName
         songName.text = track.trackName
+        
+        NetworkManager.shared.fetchImage(with: track) { result in
+            switch result {
+            case .success(let imageData):
+                DispatchQueue.main.async {
+                    self.imageSong.image = UIImage(data: imageData)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
         
     }
 }
